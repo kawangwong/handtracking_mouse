21684f5193057124d903596747498e18181e1a00
kawangwong <nycazn724@gmail.com>
2021-10-26 17:10:29 -0400

    Add files via upload

---
+++ videosetting.py
@@ -0,0 +1,7 @@
+def vidset(video, cv2):
+  video.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
+  width = 1920
+  height = 1080
+  video.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+  video.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+  video.set(cv2.CAP_PROP_FPS, 30)
