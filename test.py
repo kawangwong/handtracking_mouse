520090971ed879938244826ab05eefe63db5d0e8
kawangwong <nycazn724@gmail.com>
2021-10-27 16:58:35 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import cv2
 import mediapipe as mp
 import time
-from usersetting import customVideoCapture
+from anatomicalocation import anatomical_coordinates
+from usersetting import customVideoCapture, anatomyfinder, drawAll
 from videosetting import vidset
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -22,16 +23,21 @@ while video.isOpened():
     #reference to video input
   image.flags.writeable = False
   results = hands.process(image)
+  image.flags.writeable = True
   imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-  #converts BGR as openCV reads BGRto RGB
+  #converts BGR as openCV reads BGR to RGB
   if results.multi_hand_landmarks:
     for hand_landmarks in results.multi_hand_landmarks:
-       mp_drawing.draw_landmarks(
-         image,
-         hand_landmarks,
-         mp_hands.HAND_CONNECTIONS,
-         mp_drawing_styles.get_default_hand_landmarks_style(),
-         mp_drawing_styles.get_default_hand_connections_style())
+      if anatomyfinder == True:
+        for jointName in mp_hands.HandLandmark: ##this line was placed here instead of part of function because of performance issues.
+          anatomical_coordinates(mp_drawing, hand_landmarks, image, jointName) 
+      if drawAll == True:
+        mp_drawing.draw_landmarks(
+          image,
+          hand_landmarks,
+          mp_hands.HAND_CONNECTIONS, ##line here draws connections between joints.
+          mp_drawing_styles.get_default_hand_landmarks_style(),
+          mp_drawing_styles.get_default_hand_connections_style())
           #will draw lines and show landmarks.
       """ reference https://github.com/google/mediapipe/blob/master/mediapipe/modules/hand_landmark/hand_landmark_tracking_cpu.pbtxt"""
   cv2.imshow("Hand Tracker", cv2.flip(image, 1))
