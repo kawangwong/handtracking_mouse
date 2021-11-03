1bbb0630a1c09c92c4ce3badde3bfe4c4ca8e7c2
Kawang Wong <nycazn724@gmail.com>
2021-11-03 18:09:45 -0400

    Update usersetting.py

--- usersetting.py
+++ usersetting.py
@@ -1,6 +1,10 @@
 customVideoCapture = False
-anatomyfinder = True
+#this will turn on the function for adjusting resolution input from the camera. if you camera can do 1080 and your CPU can handle it, so be it.
 drawAll = True
+#this will draw the landmarks on a camera input
 buttonFunction = True
+#this will allow clicking if you want clicking function from the keyboard
 showCamerafeed = True
+#This will show the camera input on the screen
 buttonLetter = "e"
+#This will change the button mapping. Please use letter or number, not punctuations or special letters.
